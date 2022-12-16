a1f168f2f53969e1db61ddba1a9370fbd3f53b62
AquaticTyphoon <aquatictyphoon@yahoo.com>
2022-12-16 12:22:23 -0600

    Pokeball init Shaking

--- src/main/java/com/aquatictyphoon/pokemonmod/setup/entities/PokeballEntity.java
+++ src/main/java/com/aquatictyphoon/pokemonmod/setup/entities/PokeballEntity.java
@@ -3,7 +3,6 @@ package com.aquatictyphoon.pokemonmod.setup.entities;
 import com.aquatictyphoon.pokemonmod.setup.entities.pokemon.PokemonEntity;
 import com.aquatictyphoon.pokemonmod.setup.pokeballs.PartyPokeballProvider;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -14,17 +13,28 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
 import static com.aquatictyphoon.pokemonmod.PokemonMod.*;
 import static com.aquatictyphoon.pokemonmod.setup.sounds.ModSoundEvents.POKE_BALL_THROWN;
-import static net.minecraft.world.entity.Entity.RemovalReason.CHANGED_DIMENSION;
 
 
 public class PokeballEntity extends ThrowableItemProjectile {
 
+
     PokemonEntity CurrentMon;
     Boolean isFullBall = false;
     Entity pPlayer;
+    PokemonEntity TargetCapture;
+    boolean didCatchMon = false;
+    boolean ballShaking = false;
+    int catchTimer;
+    int onceShakeTime = 35;
+
+    int ballShakeCount = 0;
 
     public PokeballEntity(EntityType<PokeballEntity> type, Level level) {
         super(type, level);
@@ -37,61 +47,118 @@ public class PokeballEntity extends ThrowableItemProjectile {
         pPlayer.getCapability(PartyPokeballProvider.PLAYER_PARTY).ifPresent(partyStorage -> {
             CurrentMon = partyStorage.getPokemonBySlot(partyStorage.currentSlot);
         });
+        catchTimer = 0;
     }
     @Override
     protected void onHitEntity(EntityHitResult result) {
         super.onHitEntity(result);
-        if (!level.isClientSide) {
+        if (!level.isClientSide && !ballShaking) {
             if (result.getEntity() instanceof PokemonEntity pTarget ) {
                 if (pTarget.getSpecies() == 0) {return;}
 
                 if(!isFullBall){
+                    ballShaking = true;
+                    TargetCapture = pTarget;
                     float maxHP = pTarget.getMaxHealth();
                     float currentHP = pTarget.getHealth();
                     float ballBonus = 1;  // Need to program Status and Ball Bonus
                     float statusBonus = 1;
                     int catchrate = pTarget.getCatchrate();
                     float calculatedCatch = ((((((3 * (maxHP)) - 2 * (currentHP)) * catchrate * ballBonus) / 3 * maxHP) * statusBonus) / 255);
-                    if (pPlayer instanceof Player player) {
                         if (!pTarget.isTame()) {
                             if ((pTarget.isAlive()) && random.nextInt(256) <= calculatedCatch) {
-                                pTarget.tame(player);
-                                player.getCapability(PartyPokeballProvider.PLAYER_PARTY).ifPresent(partyStorage -> {
-                                    partyStorage.addPokemon(pTarget);
-                                    if(partyStorage.playerParty.stream().count() < 5){
-                                        player.displayClientMessage(Component.translatable(player.getGameProfile().getName() + " caught the wild " + pTarget.getPokeName() + " and was sent to the PC!" ), false);
-                                    }else{
-                                        player.displayClientMessage(Component.translatable(player.getGameProfile().getName() + " caught the wild " + pTarget.getPokeName() + "!"), false);
-                                    }
-                                });
-
-                                pTarget.remove(CHANGED_DIMENSION);
+                                didCatchMon = true;
                             }
                             if ((pTarget.isAlive()) && random.nextInt(256) > calculatedCatch) {
-                                player.displayClientMessage(Component.translatable("Oh no! The " + pTarget.getPokeName() + " broke free!"), true);
+                                didCatchMon = true;
                             }
                         }
-                    }
                 }else{
                     CurrentMon.absMoveTo(result.getLocation().x, result.getLocation().y + 0.5f, result.getLocation().z, 0, 0);
                     level.addFreshEntity(CurrentMon);
                     DiscardBall();
                 }
             }
-            DiscardBall();
         }
+    }
+
+
+
+    @Override
+    public void tick() {
+        super.tick();
+        System.out.println(ballShakeCount);
+        if(ballShaking && !this.isNoGravity()) {
+            ballShakeCount = random.nextInt(2);
+        }
+
+        if(ballShaking && catchTimer < onceShakeTime  + (ballShakeCount * onceShakeTime)){
+            Random shakeCounter = new Random();
+            catchTimer +=1;
+
+            this.setNoGravity(true);
+            this.setDeltaMovement(0, 0, 0);
+
+            if(TargetCapture != null) {
+                this.setPos(new Vec3(TargetCapture.position().x, TargetCapture.position().y +   0.035f, TargetCapture.position().z));
+                TargetCapture.setNoGravity(true);
+                TargetCapture.setDeltaMovement(0, 0, 0);
+                TargetCapture.setNoAi(true);
+                TargetCapture.setInvisible(true);
+            }
+        }
+
+        if(catchTimer >= (onceShakeTime  + (ballShakeCount * onceShakeTime)) && TargetCapture != null && ballShaking){
+            if(didCatchMon) {
+                doCapture();
+            }else{
+                doFailCapture();
+            }
+            TargetCapture.setInvisible(false);
+            TargetCapture.setNoAi(false);
+            TargetCapture.setNoGravity(false);
+        }
+    }
+
+    void doCapture(){
+        Player player = (Player) pPlayer;
+        if(TargetCapture != null){
+            TargetCapture.tame(player);
+            player.getCapability(PartyPokeballProvider.PLAYER_PARTY).ifPresent(partyStorage -> {
+                partyStorage.addPokemon(TargetCapture);
+                if (partyStorage.playerParty.size() > 5) {
+                    player.displayClientMessage(Component.translatable(player.getGameProfile().getName() + " caught the wild " + TargetCapture.getPokeName() + "!"), false);
+                    player.displayClientMessage(Component.translatable(TargetCapture.getPokeName() + " was sent to the PC!"), false);
+                } else {
+                    player.displayClientMessage(Component.translatable(player.getGameProfile().getName() + " caught the wild " + TargetCapture.getPokeName() + "!"), false);
+                }
+            });
+            TargetCapture.remove(RemovalReason.CHANGED_DIMENSION);
+        }
+        ballShaking = false;
+        DiscardBall();
+    }
+
+    void doFailCapture(){
+        Player player = (Player) pPlayer;
+        player.displayClientMessage(Component.translatable("Oh no! The " + TargetCapture.getPokeName() + " broke free!"), true);
+        didCatchMon = false;
+        ballShaking = false;
+        DiscardBall();
     }
 
     @Override
     protected void onHitBlock(BlockHitResult result) {
-        if (!level.isClientSide) {
-            if(CurrentMon != null && isFullBall) {
-                CurrentMon.revive();
-                CurrentMon.absMoveTo(result.getLocation().x, result.getLocation().y + 0.5f, result.getLocation().z, 0, 0);
-                level.addFreshEntity(CurrentMon);
+        if(ballShaking) {
+            if (!level.isClientSide) {
+                if (CurrentMon != null && isFullBall) {
+                    CurrentMon.revive();
+                    CurrentMon.absMoveTo(result.getLocation().x, result.getLocation().y + 0.5f, result.getLocation().z, 0, 0);
+                    level.addFreshEntity(CurrentMon);
+                }
             }
+            DiscardBall();
         }
-        DiscardBall();
     }
 
     void DiscardBall(){
