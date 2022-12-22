283125978e5e8b5636c54b364c09d1cf6c3d038c
LaDestitute <lad3stitute@gmail.com>
2022-12-22 00:30:37 -0800

    UI Fix

--- src/main/java/com/aquatictyphoon/pokemonmod/setup/client/ui/PartyHud.java
+++ src/main/java/com/aquatictyphoon/pokemonmod/setup/client/ui/PartyHud.java
@@ -8,16 +8,18 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
 public class PartyHud {
-    private static final ResourceLocation PARTY_UI = new ResourceLocation(PokemonMod.MODID, "textures/gui/party_ui.png");
+    private static final ResourceLocation PARTY_UI = new ResourceLocation(PokemonMod.MODID,
+            "textures/gui/party_ui.png");
+
     public static final IGuiOverlay HUD_PARTY = ((gui, poseStack, partialTick, width, height) -> {
-        int xPos = width / 2;
-        int yPos = height;
-        int xSize = 32 ;
-        int ySize = 193;
+        int x = width / 2;
+        int y = height;
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, PARTY_UI);
-        GuiComponent.blit(poseStack, xPos, yPos, 0, 0, xSize, ySize, xSize, ySize);
+        //You may need to fiddle with the x/y values to get it where ya want
+        GuiComponent.blit(poseStack,x - 134, y - 204,0,0,32,193,
+                32,193);
 
     });
 }
